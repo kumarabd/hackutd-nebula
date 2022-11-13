c786a39be19602295ff463139315624ef494f0ac
kumarabd <abishekkumar92@gmail.com>
2022-11-12 19:26:26 -0600

    ui
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,44 +1,58 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import info
 import models
-
+from flask_caching import Cache
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CORS_HEADERS":'Content-Type'
+}
+app.config.from_mapping(config)
+cache = Cache(app)
 
 @app.route("/api/info", methods=['GET'])
 @cross_origin()
+@cache.cached(timeout=500)
 def get_info():
     obj = info.Info()
     return obj.get_courses()
 
 @app.route("/api/health", methods=['GET'])
+@cache.cached(timeout=500)
 @cross_origin()
 def health():
     return "ok"
     
 @app.route("/api/calender/members", methods=['GET'])
 @cross_origin()
+@cache.cached(timeout=500)
 def list_calender_members():
     obj = info.Info()
     professor_objs = obj.get_professors()
+    cache.set('professors', professor_objs.data)
     plist = [{x._id:x.first_name+' '+x.last_name} for x in professor_objs.data]
     professor_objs.data = plist
     return professor_objs.toJSON()
 
 @app.route("/api/calender/members/<id>", methods=['GET'])
 @cross_origin()
+@cache.cached(timeout=500)
 def list_time_slots(id):
     obj = info.Info()
-    return obj.get_calender(id).toJSON()
+    return obj.get_calender(cache.get('professors'), id).toJSON()
 
 @app.route("/api/calender/members/<id>", methods=['POST'])
 @cross_origin()
+@cache.cached(timeout=500)
 def book_time_slots(id):
     obj = info.Info()
-    return obj.get_professors().toJSON()
+    print(request.data)
+    return models.Result('ok', '')
     
 
 if __name__ == "__main__":
--- backend/server/info.py
+++ backend/server/info.py
@@ -6,14 +6,9 @@ import models
 API_URL = 'https://api.utdnebula.com'
 HEADERS = {'content-type': 'application/json', 'x-api-key': os.environ['API_KEY']}
 
-# Store
-professor_objs = []
-
 def remove_v(dct):
     if '__v' in dct:
         dct.pop('__v', None)
-    if 'office_hours' in dct:
-        print(dct['office_hours'])
     return dct 
 
 class Info:
@@ -26,24 +21,44 @@ class Info:
             return models.Result('', str(err))
         
     def get_professors(self):
+        professor_objs = []
+        #Add dummy professor
+        dummy_office = {
+            "building": "ecs",
+            "room": "2.308",
+            "map_uri": "none"
+        }
+        dummy_office_hours = {
+            "start_date": "January 18, 2022",
+            "end_date": "May 5, 2022",
+            "meeting_days": "Monday",
+            "start_time": "10:00am",
+            "end_time": "11:15am",
+            "modality": "traditional",
+            "building": "SLC",
+            "room": "2.203"
+        }
+        dummy_sections = []
+        dummy_titles = []
+        dummy_prof = models.Professor("000000000000000000000000", "dum", "my", dummy_titles, "dummy@dummy.com", "0000000000", "none", "none", dummy_office, dummy_office_hours, dummy_sections)
+        professor_objs.append(dummy_prof)
         try:
             response = requests.get(API_URL+'/professor/',headers=HEADERS)
             response.raise_for_status()
             j = json.loads(response.text,object_hook=remove_v)
-            professor_objs = []
             for mem in j['data']:
                 professor_objs.append(models.Professor(**mem))
             return models.Result(professor_objs, '')
         except requests.exceptions.HTTPError as err:
             return models.Result('', str(err))
     
-    def get_calender(self, id):
+    def get_calender(self, professor_objs, id):
         try:
-            professor_objs = self.get_professors().data
+            if not professor_objs:
+                professor_objs = self.get_professors().data
             for mem in professor_objs:
+                print(mem)
                 if mem._id == id:
-                    print(mem.first_name)
-                    print(mem.office_hours)
                     return models.Result(mem.office_hours, '')
             return models.Result('', 'No data')
         except requests.exceptions.HTTPError as err:
